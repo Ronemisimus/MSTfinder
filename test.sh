7ac5f61a9bb895b5ca6f94b8094b8c0823807e37
Ronen Margolin <roni948576@gmail.com>
2022-05-03 16:18:27 +0300

    tests work

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 g++ Node.cpp NodeTest.cpp -o NodeTest
-valgrind ./NodeTest > NodeTest.res
+valgrind ./NodeTest 2> NodeTest.res
 
 g++ Node.cpp list.cpp ListTest.cpp -o ListTest
-valgrind ./ListTest > ListTest.res
+valgrind ./ListTest 2> ListTest.res
 
 
