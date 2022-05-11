03768d645519ed5ede8b3a7fde6b462f43e2eff8
Ronen Margolin <roni948576@gmail.com>
2022-05-11 23:32:34 +0300

    tests updated to work for hashmap and orginized - test still not passing in main

--- test.sh
+++ test.sh
@@ -10,19 +10,20 @@ valgrind ./EdgeTest 2> EdgeTest.res
 g++ -g edge.cpp Node.cpp list.cpp graph.cpp GraphTest.cpp -o GraphTest
 valgrind ./GraphTest 2> GraphTest.res
 
-g++ -g unionFind.cpp Node.cpp edge.cpp graph.cpp list.cpp main.cpp -o mainTests/mainTest
+g++ -g hashMap.cpp heap.cpp heapTest.cpp -o heapTest
+valgrind ./heapTest 2> heapTest.res
 
+g++ -g unionFind.cpp unionFindTest.cpp -o unionFindTest
+valgrind ./unionFindTest 2> unionFindTest.res
+
+g++ -g hashMap.cpp hashMapTest.cpp -o hashMapTest
+valgrind ./hashMapTest 2> hashMapTest.res
+
+g++ -g unionFind.cpp heap.cpp Node.cpp edge.cpp graph.cpp list.cpp main.cpp -o mainTests/mainTest
 valgrind mainTests/mainTest 2> mainTests/mainTest.res 
 
 for f in $(ls -1 tests)
 do
     valgrind --leak-check=full mainTests/mainTest tests/$f 2> mainTests/mainTest$f.res
 done
-
-g++ -g heap.cpp heapTest.cpp -o heapTest
-valgrind ./heapTest 2> heapTest.res
-
-g++ -g unionFind.cpp unionFindTest.cpp -o unionFindTest
-valgrind ./unionFindTest 2> unionFindTest.res
-
 
