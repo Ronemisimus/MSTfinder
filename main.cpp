894c108e56a9e9e45a9d1d002c8d685f34c8bd2a
Ronen Margolin <roni948576@gmail.com>
2022-05-12 20:53:53 +0300

    main comment for program details

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include "main.h"
 
-
+/**
+ * this program finds weights of MST in undirected graphs
+ * the graph should be in a file
+ * 
+ * Usage: ./main graph.txt
+ *  
+ * the file should contain:
+ * 1. 2 numbers: n - number of vertexes, m - number of edges
+ * 2. a list of valid edges, each in the form u v w:
+ *      * u - a vertex between 1-n
+ *      * v - a vertex between 1-n
+ *      * w - weight of edge (whole numbers)
+ * 3. 2 numbers u v:
+ *  this is an edge to remove from the graph - does not have to be an edge in the previous list
+ * 
+ * the program runs kruskal once
+ * then prim
+ * then removes the edge and runs kruskal
+ * 
+ * @author Ronen Margolin
+ */
 int main(int argc, char* argv[])
 {
     
